40204f4a22f71ad768926c9a14e6e6160e05e344
kungbaman <natchapol.kamo@hotmail.com>
2022-12-08 17:39:31 +0700

    Calculate an exchange currency on script.js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const currencyTwo = document.getElementById('currency__two')
 const inputOne = document.getElementById('amount__one')
 const inputTwo = document.getElementById('amount__two')
 
-const btn = document.getElementById('btn')
+const btnSwap = document.getElementById('btn')
 const rateText = document.getElementById('rate')
 
 currencyOne.addEventListener('change',calculatedCurrency)
 currencyTwo.addEventListener('change',calculatedCurrency)
+
+inputOne.addEventListener('input',calculatedCurrency)
+inputTwo.addEventListener('input',calculatedCurrency)
 
 function calculatedCurrency() {
     const one = currencyOne.value;
@@ -19,7 +22,16 @@ function calculatedCurrency() {
         // console.log(data.rates[two]);
         const rate = data.rates[two];
         rateText.innerHTML = `1 ${one} = ${rate} ${two}`
+        inputTwo.value = (inputOne.value*rate).toFixed(2);
+        
     })
 }
+
+btnSwap.addEventListener('click', () => {
+    const tempCurrency = currencyOne.value; // เก็บค่าต้นทาง
+    currencyOne.value = currencyTwo.value;
+    currencyTwo.value = tempCurrency;
+    calculatedCurrency();
+})
 
 calculatedCurrency();
